afa750881f5751a07571cab906b00cbfe817a1e4
Roland Ormrod <roland.ormrod@googlemail.com>
2014-07-29 13:29:26 +0100

    Adding EMA calculator and test

---
+++ GraphAnalyser.Tests/Processing/Calculators/ExponentialMovingAverageTests.cs
@@ -0,0 +1,49 @@
+﻿namespace GraphAnalyser.Tests.Processing.Calculators
+{
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using StockGraphAnalyser.Processing.Calculators;
+
+    public class ExponentialMovingAverageTests
+    {
+        [Test]
+        public void EmaTest() {
+            var closingPrices = new Dictionary<DateTime, decimal>
+                {
+                    {new DateTime(2010, 3, 24), 22.27m},
+                    {new DateTime(2010, 3, 25), 22.19m},
+                    {new DateTime(2010, 3, 26), 22.08m},
+                    {new DateTime(2010, 3, 29), 22.17m},
+                    {new DateTime(2010, 3, 30), 22.18m},
+                    {new DateTime(2010, 3, 31), 22.13m},
+                    {new DateTime(2010, 4, 1), 22.23m},
+                    {new DateTime(2010, 4, 5), 22.43m},
+                    {new DateTime(2010, 4, 6), 22.24m},
+                    {new DateTime(2010, 4, 7), 22.29m},
+                    {new DateTime(2010, 4, 8), 22.15m},
+                    {new DateTime(2010, 4, 9), 22.39m},
+                    {new DateTime(2010, 4, 10), 22.38m},
+                    {new DateTime(2010, 4, 11), 22.61m},
+                    {new DateTime(2010, 4, 12), 23.36m},
+                    {new DateTime(2010, 4, 13), 24.05m}
+                };
+
+            var expectedResult = new Dictionary<DateTime, decimal>
+                {
+                    {new DateTime(2010, 4, 7), 22.22m},
+                    {new DateTime(2010, 4, 8), 22.21m},
+                    {new DateTime(2010, 4, 9), 22.24m},
+                    {new DateTime(2010, 4, 10), 22.27m},
+                    {new DateTime(2010, 4, 11), 22.33m},
+                    {new DateTime(2010, 4, 12), 22.52m},
+                    {new DateTime(2010, 4, 13), 22.80m}
+                };
+
+
+            var emaCalculator = new ExponentialMovingAverageCalculator(closingPrices, 10);
+            var result = emaCalculator.Calculate().Result;
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
--- StockGraphAnalyser/Processing/Calculators/ExponentialMovingAverageCalculator.cs
+++ StockGraphAnalyser/Processing/Calculators/ExponentialMovingAverageCalculator.cs
@@ -4,34 +4,41 @@ namespace StockGraphAnalyser.Processing.Calculators
 {
     using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
     using System.Linq;
+    using System.Threading.Tasks;
 
     public class ExponentialMovingAverageCalculator :ICalculationTool
     {
-        private readonly int sampleSize;
-        private Dictionary<DateTime, decimal> datapoints;
+        private readonly int timePeriod;
+        private readonly Dictionary<DateTime, decimal> closingPrices;
 
-        public ExponentialMovingAverageCalculator(int sampleSize, Dictionary<DateTime, decimal> datapoints) {
-            this.sampleSize = sampleSize;
-            this.datapoints = datapoints;
+        public ExponentialMovingAverageCalculator(Dictionary<DateTime, decimal> closingPrices, int timePeriod) {
+            this.timePeriod = timePeriod;
+            this.closingPrices = closingPrices;
         }
 
-
         public Task<Dictionary<DateTime, decimal>> Calculate() {
-         
-            //var multiplier = 2 / (this.sampleSize + 1);
-            //var ema = new Dictionary<DateTime, decimal>();
 
-            //foreach (var datapoint in this.datapoints)
-            //{
-            //    var yesterdayEma = ema.LastOrDefault();
-            //    datapoint.Value * multiplier + (yesterdayEma == KeyValuePair. ? 0 : yesterdayEma.Value) * (1 – multiplier);
-            //}
+            return Task.Run(() =>
+                {
+                    var orderedClosingPrices = this.closingPrices.OrderBy(p => p.Key);
+                    var startingEmaValue = Math.Round(orderedClosingPrices.Take(this.timePeriod).Average(e => e.Value), 2);
+                    var startingEmaDate = orderedClosingPrices.Skip(timePeriod - 1).First().Key;
 
-            //return 
+                    var multiplier = (decimal)Math.Round(2 / (decimal)(this.timePeriod + 1), 2);
+                    var ema = new Dictionary<DateTime, decimal> { { startingEmaDate, startingEmaValue } };
 
-            return null;
+                    var closingPricesToWorkWith =
+                        this.closingPrices.Where(e => e.Key > startingEmaDate).OrderBy(e => e.Key);
+
+                    foreach (var closingPrice in closingPricesToWorkWith)
+                    {
+                        var yesterdayEma = ema.LastOrDefault();
+                        ema.Add(closingPrice.Key, Math.Round(((closingPrice.Value - yesterdayEma.Value)*multiplier + yesterdayEma.Value), 2));
+                    }
+
+                    return ema;
+                });
         }
     }
 }

e5cd7cddc6f27a8ea82b457080e76f04da9b1bfd
Roland Ormrod <roland.ormrod@googlemail.com>
2014-09-15 16:19:50 +0100

    Cleaning up unit test

--- GraphAnalyser.Tests/Signals/SignalEquityPositionTotallerTests.cs
+++ GraphAnalyser.Tests/Signals/SignalEquityPositionTotallerTests.cs
@@ -3,10 +3,8 @@ namespace GraphAnalyser.Tests.Signals
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using NUnit.Framework;
     using StockGraphAnalyser.Domain;
-    using StockGraphAnalyser.Domain.Repository;
     using StockGraphAnalyser.Signals;
 
     [TestFixture]
@@ -101,44 +99,6 @@ namespace GraphAnalyser.Tests.Signals
                                                                       {DateTime.Today.AddDays(1), 1800}
                                                                   };
             Assert.AreEqual(expectedResult, result);
-        }
-
-        [Test]
-        public void ForALaugh()
-        {
-            var repository = new TradeSignalRepository();
-            var companies = new CompanyRepository().FindByIndex(Company.ConstituentOfIndex.SmallCap);
-            var totalEquityUsed = 0m;
-            var totalEndEquity = 0m;
-            foreach (var company in companies)
-            {
-                var signals = repository.GetAllForCompany(company.Symbol).OrderBy(d => d.Date);
-                if (signals.Any())
-                {
-                    var results = new SignalEquityPositionTotaller(repository.GetAllForCompany(company.Symbol).OrderBy(d => d.Date), 100).Calculate();
-                    if (results.OrderBy(d => d.Key).Last().Value < 700 && results.OrderBy(d => d.Key).Last().Value > -700)
-                    {
-                        totalEquityUsed = totalEquityUsed + 100;
-                        totalEndEquity = totalEndEquity + results.OrderBy(d => d.Key).Last().Value;
-                        Console.WriteLine("For company {0}. Start Equity {1}. End Equity {2}", company.Symbol, 100, Math.Round(results.OrderBy(d => d.Key).Last().Value, 2));
-                    }
-                }
-            }
-
-            Console.WriteLine("Total Equity Used = {0}. End Result = {1}", totalEquityUsed, totalEndEquity);
-        }
-
-        [Test]
-        public void ForALaugh2()
-        {
-            var repository = new TradeSignalRepository();
-            var trades = repository.GetAllForCompany("BDEV.L").OrderBy(d => d.Date);
-            var totals = new SignalEquityPositionTotaller(trades, 100).Calculate();
-            foreach (var total in totals)
-            {
-                var trade = trades.First(t => t.Date == total.Key);
-                Console.WriteLine("{0}: {1} at {2}. Current Equity {3}", total.Key, trade.SignalType, trade.Price, Math.Round(total.Value, 2));
-            }
         }
     }
 }

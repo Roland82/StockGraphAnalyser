2dfd7e326e79940a5dc1634f5e19d1ae31fce9f1
Roland Ormrod <roland.ormrod@googlemail.com>
2014-09-18 20:15:26 +0100

    Missing files from last commit

--- GraphAnalyser.Tests/Signals/HighMomentumTradingStrategyTests.cs
+++ GraphAnalyser.Tests/Signals/HighMomentumTradingStrategyTests.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace GraphAnalyser.Tests.Signals
 {
-    class HighMomentumTradingStrategyTests
+    using NUnit.Framework;
+
+    public class HighMomentumTradingStrategyTests
     {
+        [TestCase(1, 2, 3, 4, 5, true)]
+        [TestCase(1m, 1.5m, 2m, 2.5m , 3m, true)]
+        public void IsApplicableToTests(decimal maDay1, decimal maDay2, decimal maDay3, decimal maDay4, decimal maDay5, bool expResult)  {
+            v
+        }
     }
 }
--- StockGraphAnalyser/Signals/HighMomentumTradingStrategy.cs
+++ StockGraphAnalyser/Signals/HighMomentumTradingStrategy.cs
@@ -1,7 +1,26 @@
 ﻿
 namespace StockGraphAnalyser.Signals
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using StockGraphAnalyser.Domain;
+    using StockGraphAnalyser.Processing;
+
     public class HighMomentumTradingStrategy
     {
+        public bool IsApplicableTo(IEnumerable<DataPoints> dataPoints) {
+             var numberOfDataPoints = dataPoints.Count();
+             var average50DayMaRise = dataPoints
+                .OrderBy(d => d.Date)
+                .Skip(numberOfDataPoints - 5)
+                .Take(5)
+                .ForEachGroup(2, e => MathExtras.PercentageDifferenceBetween(e.ElementAt(0).MovingAverageFiftyDay.Value, e.ElementAt(1).MovingAverageFiftyDay.Value))
+                .Average();
+
+            return average50DayMaRise > 0.5m;
+        }
+
+
+
     }
 }
